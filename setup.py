74c6125ef651cd1faa38f5e3184d3a23d6e20c78
Chris Lumens <clumens@redhat.com>
2015-02-23 15:07:36 -0500

    Require pylint to be installed.

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ from distutils.core import setup
 setup(name='pocketlint', version='1',
       description='Support for running pylint against projects',
       author='Chris Lumens', author_email='clumens@redhat.com',
+      requires=['pylint'],
       packages=['pocketlint', 'pocketlint.checkers'])
