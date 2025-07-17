c5b640f75b27a2b8e8800eb983ff284e2d07688a
Vojtech Trefny <vtrefny@redhat.com>
2025-07-17 14:49:11 +0200

    Move project definitions from setup.py to pyproject.toml
    An empty setup.py is kept for backwards compatibility.

--- setup.py
+++ setup.py
@@ -2,18 +2,4 @@
 
 from setuptools import setup
 
-setup(name='pocketlint', version='0.25',
-      description='Support for running pylint against projects',
-      author='Chris Lumens', author_email='clumens@redhat.com',
-      url='https://github.com/rhinstaller/pocketlint',
-      license='COPYING',
-      install_requires=['pylint', 'polib', 'packaging'],
-      long_description=open('README', encoding='utf-8').read(),
-      packages=['pocketlint', 'pocketlint.checkers'],
-      classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Testing",
-      ],
-      )
+setup()
